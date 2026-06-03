6cd3bb964cac557d000b1985cd63bae1b85a4832
Thrusha2 <nimashethrusha269@gmail.com>
2026-06-03 10:10:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://kommodo.ai/i/CiPi4Quow7N05Rt6vOdm",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 mahasona-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
